6c35dddec1ee6ce446ed52d438bc4c1555b3edb5
Bruno Barbosa <brunobarbosa96@hotmail.com>
2018-07-09 09:33:22 -0300

    Criação e início do projeto

---
+++ ML_Challenge/src/main/java/com/mlchallenge/api/MlChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.mlchallenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlChallengeApplication.class, args);
+	}
+}
